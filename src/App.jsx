6472be87714512765638b7b1ea65aa01cf9c5a2d
Niranjan Gaonkar <niranjangaonkar.09@gmail.com>
2023-04-09 23:17:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import InsertForm from './Components/InsertionForm'
+// import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <InsertForm/>
+    </div>
+  )
+}
+
+export default App
